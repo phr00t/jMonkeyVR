f0c3ff019a0c142c7d0aac2fbbb72874d9eb31a3
rickardeden@gmail.com <rickardeden@gmail.com@5837e5f2-d02d-a326-8f71-82a73a3188da>
2013-10-31 19:35:53 +0000

    c++ source for the dll

---
+++ cpp/oculusvr_input_OculusRift.cpp
@@ -0,0 +1,125 @@
+#include <jni.h>  
+ #include <iostream>  
+ #include <stdio.h>  
+ #include "OVR.h"  
+ #include "oculusvr_input_OculusRift.h"  
+ using namespace OVR;  
+ using namespace std;  
+ Ptr<DeviceManager>     pManager;  
+ Ptr<HMDDevice>     pHMD;  
+ Ptr<SensorDevice>     pSensor;  
+ SensorFusion     FusionResult;  
+ HMDInfo     Info;  
+ bool InfoLoaded;  
+ //#define STD_GRAV 9.81 // What SHOULD work with Rift, but off by 1000  
+ #define STD_GRAV 0.00981 // This gives nice 1.00G on Z with Rift face down !!!  
+ JNIEXPORT jboolean JNICALL Java_oculusvr_input_OculusRift_initialize  
+  (JNIEnv *env, jobject thisObj) {  
+   printf("Initializing Rift...\n");  
+   System::Init();  
+   bool initialized = false;  
+      pManager = *DeviceManager::Create();  
+      pHMD = *pManager->EnumerateDevices<HMDDevice>().CreateDevice();  
+      if (pHMD){  
+           printf("pHMD created\n");  
+           InfoLoaded = pHMD->GetDeviceInfo(&Info);  
+           pSensor = *pHMD->GetSensor();  
+      }  
+      else{  
+           pSensor = *pManager->EnumerateDevices<SensorDevice>().CreateDevice();  
+      }  
+      if (pSensor)  
+      {  
+           printf("Attaching sensor\n");  
+           FusionResult.AttachToSensor(pSensor);  
+     initialized = true;  
+      }  
+   return initialized;  
+ }  
+ JNIEXPORT void JNICALL Java_oculusvr_input_OculusRift_destroy  
+      (JNIEnv *env, jobject thisObj) {  
+           pSensor.Clear();  
+     pHMD.Clear();  
+           pManager.Clear();  
+           System::Destroy();  
+		   printf("Cleaning up\n");  
+           return;  
+ }  
+ JNIEXPORT jfloatArray JNICALL Java_oculusvr_input_OculusRift_update  
+  (JNIEnv *env, jobject thisObj) {  
+   jfloat data[6];  
+   Vector3f acc=FusionResult.GetAcceleration();  
+   // yaw, pitch, roll  
+   FusionResult.GetOrientation().GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&data[2], &data[1], &data[0]);  
+   data[3] = acc.x / STD_GRAV;  
+   data[4] = acc.y / STD_GRAV;  
+   data[5] = acc.z / STD_GRAV;  
+   jfloatArray result;  
+   result = env->NewFloatArray(6);  
+   if (result == NULL) {  
+     return NULL; /* out of memory error thrown */  
+   }  
+   env->SetFloatArrayRegion(result, 0, 6, data);  
+   return result;  
+ }  
+ JNIEXPORT jint JNICALL Java_oculusvr_input_OculusRift_getHResolution  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.HResolution;  
+ }  
+ JNIEXPORT jint JNICALL Java_oculusvr_input_OculusRift_getVResolution  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.VResolution;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getHScreenSize  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.HScreenSize;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getVScreenSize  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.VScreenSize;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getVScreenCenter  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.VScreenCenter;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getEyeToScreenDistance  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.EyeToScreenDistance;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getLensSeparationDistance  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.LensSeparationDistance;  
+ }  
+ JNIEXPORT jfloat JNICALL Java_oculusvr_input_OculusRift_getInterpupillaryDistance  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.InterpupillaryDistance;  
+ }  
+ JNIEXPORT jfloatArray JNICALL Java_oculusvr_input_OculusRift_getDistortionK  
+  (JNIEnv *env, jobject thisObj) {  
+   jfloat* distortion = Info.DistortionK;  
+   jfloatArray result;  
+   result = env->NewFloatArray(6);  
+   if (result == NULL) {  
+     return NULL; /* out of memory error thrown */  
+   }  
+   env->SetFloatArrayRegion(result, 0, 6, distortion);  
+   return result;  
+ }  
+ JNIEXPORT jint JNICALL Java_oculusvr_input_OculusRift_getDesktopX  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.DesktopX;  
+ }  
+ JNIEXPORT jint JNICALL Java_oculusvr_input_OculusRift_getDesktopY  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.DesktopY;  
+ }  
+ JNIEXPORT jstring JNICALL Java_oculusvr_input_OculusRift_getDisplayDeviceName  
+  (JNIEnv *env, jobject thisObj) {  
+   char *name = Info.DisplayDeviceName;  
+   jstring result = env->NewStringUTF(name);  
+   return result;  
+ }  
+ JNIEXPORT jlong JNICALL Java_oculusvr_input_OculusRift_getDisplayId  
+  (JNIEnv *env, jobject thisObj) {  
+   return Info.DisplayId;  
+ }  

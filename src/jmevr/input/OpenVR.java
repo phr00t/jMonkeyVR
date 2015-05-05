be8c40c23ecf594074ac8d9089502374e4c74b79
Phr00t <phr00t@gmail.com>
2015-05-05 12:07:33 -0400

    better initialization & checking

--- src/jmevr/input/OpenVR.java
+++ src/jmevr/input/OpenVR.java
@@ -10,6 +10,8 @@ import com.jme3.math.Vector3f;
 import openvr_api.IVRSystem;
 import openvr_api.Openvr_apiLibrary;
 import org.bridj.Pointer;
+import org.bridj.Pointer.StringType;
+import org.bridj.PointerIO;
 
 /**
  *
@@ -30,10 +32,16 @@ public class OpenVR implements VRHMD {
     public boolean initialize() {
         Pointer l = Pointer.allocateLong();
         Pointer<IVRSystem> pvr = openvr.vRInit(l);
-        if( pvr != null ) vrsystem = pvr.get();
+        if( pvr != null ) vrsystem = pvr.get();        
         long res = l.getLong();
-        System.out.println("OpenVR Initialize Result: " + Long.toString(res));
-        return res == 0;
+        if( res != 0 ) {
+            Pointer str = Pointer.pointerToAddress(openvr.vRGetStringForHmdError(res), (PointerIO)null);
+            System.out.println("OpenVR Initialize Result: " + str.getString(StringType.C));
+            return false;
+        } else {
+            System.out.println("OpenVR initialized & VR connected.");
+            return true;
+        }
     }
 
     @Override
@@ -63,11 +71,12 @@ public class OpenVR implements VRHMD {
 
     @Override
     public boolean isInitialized() {
-        return forceInitialize || openvr.vRIsHmdPresent();
+        return forceInitialize || vrsystem != null && openvr.vRIsHmdPresent();
     }
 
     @Override
     public void reset() {
+        if( vrsystem == null ) return;
         vrsystem.resetSeatedZeroPose();
     }
 

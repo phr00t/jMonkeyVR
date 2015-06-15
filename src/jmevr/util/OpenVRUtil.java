27775f9655b5a5cdd0c768f5415a1b354330aa2d
Phr00t <phr00t@gmail.com>
2015-06-15 11:31:43 -0400

    updating to OpenVR 0.9.2, converted to JNA, using the C interface

--- src/jmevr/util/OpenVRUtil.java
+++ src/jmevr/util/OpenVRUtil.java
@@ -5,8 +5,8 @@
 package jmevr.util;
 
 import com.jme3.math.Matrix4f;
-import openvr_api.HmdMatrix34_t;
-import openvr_api.HmdMatrix44_t;
+import jopenvr.HmdMatrix34_t;
+import jopenvr.HmdMatrix44_t;
 
 /**
  *
@@ -16,19 +16,19 @@ public class OpenVRUtil {
     
     public static Matrix4f convertSteamVRMatrix3ToMatrix4f(HmdMatrix34_t hmdMatrix, Matrix4f mat){
         //TODO: verify that this is a correct assumption;
-        mat.set(hmdMatrix.m().get(0), hmdMatrix.m().get(1), hmdMatrix.m().get(2), 0f, 
-                hmdMatrix.m().get(3), hmdMatrix.m().get(4), hmdMatrix.m().get(5), 0f, 
-                hmdMatrix.m().get(6), hmdMatrix.m().get(7), hmdMatrix.m().get(8), 0f, 
-                hmdMatrix.m().get(9), hmdMatrix.m().get(10), hmdMatrix.m().get(11), 1f);
+        mat.set(hmdMatrix.m[0], hmdMatrix.m[1], hmdMatrix.m[2], 0f, 
+                hmdMatrix.m[3], hmdMatrix.m[4], hmdMatrix.m[5], 0f, 
+                hmdMatrix.m[6], hmdMatrix.m[7], hmdMatrix.m[8], 0f, 
+                hmdMatrix.m[9], hmdMatrix.m[10], hmdMatrix.m[11], 1f);
         return mat;
     }
     
     public static Matrix4f convertSteamVRMatrix4ToMatrix4f(HmdMatrix44_t hmdMatrix, Matrix4f mat){
         //TODO: verify that this is a correct assumption;
-        mat.set(hmdMatrix.m().get(0), hmdMatrix.m().get(1), hmdMatrix.m().get(2), hmdMatrix.m().get(3), 
-                hmdMatrix.m().get(4), hmdMatrix.m().get(5), hmdMatrix.m().get(6), hmdMatrix.m().get(7),
-                hmdMatrix.m().get(8), hmdMatrix.m().get(9), hmdMatrix.m().get(10), hmdMatrix.m().get(11), 
-                hmdMatrix.m().get(12), hmdMatrix.m().get(13), hmdMatrix.m().get(14), hmdMatrix.m().get(15));
+        mat.set(hmdMatrix.m[0], hmdMatrix.m[1], hmdMatrix.m[2], hmdMatrix.m[3], 
+                hmdMatrix.m[4], hmdMatrix.m[5], hmdMatrix.m[6], hmdMatrix.m[7],
+                hmdMatrix.m[8], hmdMatrix.m[9], hmdMatrix.m[10], hmdMatrix.m[11], 
+                hmdMatrix.m[12], hmdMatrix.m[13], hmdMatrix.m[14], hmdMatrix.m[15]);
         return mat;
     }
 }

83b6c1dfac09dff8fec8871bb89a709e2adcc780
Phr00t <phr00t@gmail.com>
2015-12-22 10:06:07 -0500

    SSAO & Shadows supported in VR instancing mode now! Performance improvements to filters, added normal passes that are instancing-aware. Instancing really should be the default VR rendering method...

--- src/jmevr/post/CartoonSSAO.java
+++ src/jmevr/post/CartoonSSAO.java
@@ -5,32 +5,20 @@
 package jmevr.post;
 
 import com.jme3.asset.AssetManager;
-import com.jme3.export.InputCapsule;
-import com.jme3.export.JmeExporter;
-import com.jme3.export.JmeImporter;
-import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
-import com.jme3.renderer.Renderer;
 import com.jme3.renderer.ViewPort;
 import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.texture.Image.Format;
-import com.jme3.texture.Texture;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class CartoonSSAO extends Filter{
     private Pass normalPass;
     private Vector3f frustumCorner;
     private Vector2f frustumNearFar;
     // Wide area occlusion settings
-    private float sampleRadius = 3.0f;
-    private float intensity = 10.2f;
-    private float scale = 3.15f;
-    private float bias = 0.025f;
     private float downsample = 1f;
 
     //private float downSampleFactor = 1f;
@@ -46,27 +34,15 @@ public class CartoonSSAO extends Filter{
 
     /**
     * Create a Screen Space Ambient Occlusion Filter
-    * @param sampleRadius The radius of the area where random samples will be picked. default 3.0f
-    * @param intensity intensity of the resulting AO. default 10.2f
-    * @param scale distance between occluders and occludee. default 3.15f
-    * @param bias the width of the occlusion cone considered by the occludee. default 0.025f
     * @param downsample factor to divide resolution by for filter, >1 increases speed but degrades quality
     */
-    public CartoonSSAO(float sampleRadius, float intensity, float scale, float bias, float downsample) {
+    public CartoonSSAO(float downsample) {
         this();
-        this.sampleRadius = sampleRadius;
-        this.intensity = intensity;
-        this.scale = scale;
-        this.bias = bias;
         this.downsample = downsample;
     }
     
     public CartoonSSAO(CartoonSSAO cloneFrom) {
-        this();
-        this.sampleRadius = cloneFrom.sampleRadius;
-        this.intensity = cloneFrom.intensity;
-        this.scale = cloneFrom.scale;
-        this.bias = cloneFrom.bias;
+        this(cloneFrom.downsample);
     }
 
     @Override
@@ -92,6 +68,10 @@ public class CartoonSSAO extends Filter{
         return material;
     }
 
+    public void setDistance(float dist) {
+        material.setFloat("Distance", dist);
+    }
+    
     @Override
     protected void initFilter(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
         this.renderManager = renderManager;
@@ -115,124 +95,9 @@ public class CartoonSSAO extends Filter{
         material = new Material(manager, "jmevr/shaders/CartoonSSAO.j3md");
         material.setTexture("Normals", normalPass.getRenderedTexture());
 
-        material.setFloat("SampleRadius", sampleRadius);
-        material.setFloat("Intensity", intensity);
-        material.setFloat("Scale", scale);
-        material.setFloat("Bias", bias);
-
         material.setVector3("FrustumCorner", frustumCorner);
         material.setVector2("FrustumNearFar", frustumNearFar);
+        material.setFloat("Distance", 0.0005f);
     }
 
-    /**
-    * Return the wide area bias<br>
-    * see {@link  #setBias(float bias)}
-    * @return  bias
-    */
-    public float getBias() {
-        return bias;
-    }
-
-    /**
-    * Sets the the width of the wide area occlusion cone considered by the occludee default is 0.025f
-    * @param bias
-    */
-    public void setBias(float bias) {
-        this.bias = bias;
-        if (material != null) {
-            material.setFloat("Bias", bias);
-        }
-    }
-
-    /**
-    /**
-    * returns the ambient occlusion intensity
-    * @return intensity
-    */
-    public float getIntensity() {
-        return intensity;
-    }
-
-    /**
-    * Sets the Ambient occlusion intensity default is 10.2f
-    * @param intensity
-    */
-    public void setIntensity(float intensity) {
-        this.intensity = intensity;
-        if (material != null) {
-            material.setFloat("Intensity", intensity);
-        }
-    }
-
-
-    /**
-    * returns the sample radius<br>
-    * see {link setSampleRadius(float sampleRadius)}
-    * @return the sample radius
-    */
-    public float getSampleRadius() {
-        return sampleRadius;
-    }
-
-    /**
-    * Sets the radius of the area where random samples will be picked dafault 3.0f
-    * @param sampleRadius
-    */
-    public void setSampleRadius(float sampleRadius) {
-        this.sampleRadius = sampleRadius;
-        if (material != null) {
-            material.setFloat("SampleRadius", sampleRadius);
-        }
-    }
-
-    /**
-    * returns the scale<br>
-    * see {@link #setScale(float scale)}
-    * @return scale
-    */
-    public float getScale() {
-        return scale;
-    }
-
-    /**
-    *
-    * Returns the distance between occluders and occludee. default 3.15f
-    * @param scale
-    */
-    public void setScale(float scale) {
-        this.scale = scale;
-        if (material != null) {
-            material.setFloat("Scale", scale);
-        }
-    }
-
-
-    public void scaleSettings(float aoScale) {
-        setBias(getBias()*aoScale);
-        setIntensity(getIntensity()*aoScale);
-        setScale(getScale()*aoScale);
-        setScale(getScale()*aoScale);
-        setSampleRadius(getSampleRadius()*aoScale);
-    }
-
-    @Override
-    public void write(JmeExporter ex) throws IOException {
-        super.write(ex);
-
-        OutputCapsule oc = ex.getCapsule(this);
-            oc.write(sampleRadius, "sampleRadius", 3.0f);
-            oc.write(intensity, "intensity", 10.2f);
-            oc.write(scale, "scale", 3.15f);
-            oc.write(bias, "bias", 0.025f);
-    }
-
-    @Override
-    public void read(JmeImporter im) throws IOException {
-        super.read(im);
-        InputCapsule ic = im.getCapsule(this);
-        sampleRadius = ic.readFloat("sampleRadius", 3.0f);
-        intensity = ic.readFloat("intensity", 10.2f);
-        scale = ic.readFloat("scale", 3.15f);
-        bias = ic.readFloat("bias", 0.025f);
-    }
 }
